90ca765a75f6231cc93fd27a19ffdd82af8ba1dd
Kevin Padron <kvnnpdrn@gmail.com>
2018-03-05 06:47:49 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,6 +5,8 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return playlist
 }
 
+var playlist = {artistName: 'songTitle'}
+
 function removeFromPlaylist(playlist, artistName) {
   delete playlist.artistName
 }
